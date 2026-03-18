5e7bb70472af134b477ec7b4b9497d3d5c478bd1
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-03-18 19:34:16 -0300

    :sparkles:feat: Definição da rota inicial + método static

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:uber_flutter/firebase_options.dart';
+import 'package:uber_flutter/routes/routes.dart';
 import 'package:uber_flutter/screens/home_screen.dart';
 
 void main() async {
@@ -26,6 +27,8 @@ void main() async {
         ),
         secondaryHeaderColor: Color(0xff546e7a)
       ),
+      initialRoute: "/",
+      onGenerateRoute: Routes.generateRoute,
       
     )
   );
