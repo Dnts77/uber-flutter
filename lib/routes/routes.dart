75a36492a5bb62dfc3a73dc4773e1614d0e4b3ce
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-03-19 15:40:19 -0300

    :sparkles:feat: Novas rotas de passageiro & motorista

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:uber_flutter/screens/driver_panel.dart';
 import 'package:uber_flutter/screens/home_screen.dart';
+import 'package:uber_flutter/screens/passenger_panel.dart';
 import 'package:uber_flutter/screens/sign_up_screen.dart';
 
 class Routes{
@@ -9,6 +11,10 @@ class Routes{
         return MaterialPageRoute(builder: (_)=>HomeScreen());
       case "/cadastro":
         return MaterialPageRoute(builder: (_)=> SignUpScreen());
+      case "/painel-motorista":
+        return MaterialPageRoute(builder: (_)=> DriverPanel());
+      case "/painel-passageiro":
+        return MaterialPageRoute(builder: (_)=> PassengerPanel());
       default:
         return _routeError();
     }
