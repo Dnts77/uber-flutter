4555d9766ee818ab10cc7b4fc70dd153955e257c
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-03-19 15:45:22 -0300

    :sparkles:feat: Lógica de cadastro do usuário

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:uber_flutter/model/Usuario.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -12,7 +17,77 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _controllerEmail = TextEditingController(); // Controlador do email
   final TextEditingController _controllerNome = TextEditingController(); // Controlador do nome
   final TextEditingController _controllerSenha = TextEditingController(); // Controlador da senha
-  bool _typeUserPassenger = false;
+  bool _userType = false; //tipo de passageiro -> falso passageiro, verdadeiro motorista 
+  String _errorMessage = "";
+
+  
+  //Validação dos campos
+  void _validateFields(){
+    String nome = _controllerNome.text;
+    String email = _controllerEmail.text;
+    String senha = _controllerSenha.text;
+
+    if(nome.isNotEmpty){
+      
+      if(email.isNotEmpty && email.contains("@")){
+        
+        if(senha.isNotEmpty && senha.length > 6 ){
+
+          Usuario usuario = Usuario();
+          usuario.nome = nome;
+          usuario.email = email;
+          usuario.senha = senha;
+          usuario.tipoUsuario = usuario.checkTypeUser(_userType);
+
+          _signUpUser( usuario );
+        }
+
+        else{
+
+          setState(() {
+            _errorMessage = "Preencha a senha!";
+          });
+
+        }
+      }
+      
+      else{
+        setState(() {
+          _errorMessage = "Preencha o email!";
+        });
+      }
+    }
+    
+    else{
+      setState(() {
+        _errorMessage = "Preencha o nome!";
+      });
+
+    }
+  }
+
+
+  // Cadastrando o usuário
+  void _signUpUser(Usuario usuario){
+
+    FirebaseAuth auth = FirebaseAuth.instance;
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    auth.createUserWithEmailAndPassword(
+      email: usuario.email, 
+      password: usuario.senha
+      )
+      .then((firebaseUser){
+        db.collection("usuarios").doc(firebaseUser.user!.uid).set(usuario.toMap());
+        switch(usuario.tipoUsuario){
+          case "motorista":
+          return Navigator.pushNamedAndRemoveUntil(context, "/painel-motorista", (_)=>false);
+          case "passageiro":
+          return Navigator.pushNamedAndRemoveUntil(context, "/painel-passageiro", (_)=>false);
+        }
+      }
+    );
+
+  }
 
 
   @override
@@ -122,9 +197,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   child: Row(
                     children: [
                       Text("Passageiro"),
-                      Switch(value: _typeUserPassenger, activeThumbColor:Color(0xff1ebbd8), onChanged: (bool valor){
+                      Switch(value: _userType, activeThumbColor:Color(0xff1ebbd8), onChanged: (bool valor){
                         setState(() {
-                          _typeUserPassenger = valor;
+                          _userType = valor;
                         });
                       }),
                       Text("Passageiro")
@@ -135,7 +210,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 Padding(
                   padding: const EdgeInsets.only(top: 16, bottom: 10),
                   child: ElevatedButton(
-                    onPressed: (){},
+                    onPressed: (){
+                      _validateFields();
+                    },
                     style: const ButtonStyle(
                       backgroundColor: WidgetStatePropertyAll(
                         Color(0xff1ebbd8),
@@ -156,7 +233,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 Padding(
                   padding: const EdgeInsets.only(top: 16),
                   child: Center(
-                    child: const Text("Erro", style: TextStyle(color: Colors.red, fontSize: 20),),
+                    child: Text(_errorMessage, style: TextStyle(color: Colors.red, fontSize: 20),),
                   ),
                 )
               ],
