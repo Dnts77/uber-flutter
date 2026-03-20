f257b514a816e0e26bd9ae704749aaf0612982b6
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-03-20 18:34:21 -0300

    :sparkles:feat: Interface inicial do painel de motorista

--- lib/screens/driver_panel.dart
+++ lib/screens/driver_panel.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/material.dart'; 
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class DriverPanel extends StatefulWidget {
   const DriverPanel({super.key});
@@ -8,8 +11,49 @@ class DriverPanel extends StatefulWidget {
 }
 
 class _DriverPanelState extends State<DriverPanel> {
+
+  List<String> menuItems = [
+    "Deslogar", "Configurações"
+  ];
+
+
+  // Menu de PopUp
+  void _menuItemChoice(String escolha){
+    switch(escolha){
+      case "Deslogar":
+        _userSignOut();
+        break;
+      case "Configurações":
+
+        break;
+    }
+  }
+
+  //Deslogando o usuário
+  Future<void> _userSignOut() async{
+    FirebaseAuth auth = FirebaseAuth.instance;
+    await auth.signOut();
+    Navigator.pushReplacementNamed(context, "/");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Painel motorista"),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: _menuItemChoice,
+            itemBuilder: (context){
+              
+              return menuItems.map((String item) {
+                return PopupMenuItem<String>(value: item, child: Text(item));
+              }).toList();
+            },
+          )
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
