c5e4b80adb081ba846d0c0518b778cafb9f584c1
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-03-20 16:41:06 -0300

    :sparkles:feat: Métodos iniciais & PopUpMenuButton

--- lib/screens/passenger_panel.dart
+++ lib/screens/passenger_panel.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class PassengerPanel extends StatefulWidget {
   const PassengerPanel({super.key});
@@ -8,11 +11,48 @@ class PassengerPanel extends StatefulWidget {
 }
 
 class _PassengerPanelState extends State<PassengerPanel> {
+
+  List<String> menuItems = [
+    "Deslogar", "Configurações"
+  ];
+
+
+  // Menu de PopUp
+  void _menuItemChoice(String escolha){
+    switch(escolha){
+      case "Deslogar":
+        _userSignOut();
+        break;
+      case "Configurações":
+
+        break;
+    }
+  }
+
+  //Deslogando o usuário
+  Future<void> _userSignOut() async{
+    FirebaseAuth auth = FirebaseAuth.instance;
+    await auth.signOut();
+    Navigator.pushReplacementNamed(context, "/");
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Painel passageiro"),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: _menuItemChoice,
+            itemBuilder: (context){
+              
+              return menuItems.map((String item) {
+                return PopupMenuItem<String>(value: item, child: Text(item));
+              }).toList();
+            },
+          )
+        ],
       ),
       body: Container(),
     );
