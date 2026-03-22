b1a8b05e4daef4acfcbd51954d2f3e23709f1ce1
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-03-22 12:46:23 -0300

    :sparkles:feat: Recuperando última localização do usuário, permissões para geolocalização, movimentação da câmera, inicialização dos métodos

--- lib/screens/passenger_panel.dart
+++ lib/screens/passenger_panel.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
+import 'package:geolocator/geolocator.dart';
 
 
 class PassengerPanel extends StatefulWidget {
@@ -15,7 +16,11 @@ class PassengerPanel extends StatefulWidget {
 
 class _PassengerPanelState extends State<PassengerPanel> {
 
-  Completer<GoogleMapController> _mapController = Completer();
+  final Completer<GoogleMapController> _mapController = Completer();
+
+  CameraPosition _cameraPosition = CameraPosition(
+    target: LatLng(-23.472297, -46.530986),
+  );
 
   List<String> menuItems = [
     "Deslogar", "Configurações"
@@ -46,6 +51,80 @@ class _PassengerPanelState extends State<PassengerPanel> {
     _mapController.complete(controller);
   }
 
+  //Recuperando última localização 
+  Future<void> _getLastKnownPositon() async{
+    Position? position = await Geolocator.getLastKnownPosition();
+    setState(() {
+      if(position != null){
+        _cameraPosition = CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 19,
+        );
+        _moveCamera(_cameraPosition);
+      }
+    });
+  
+  }
+
+  //Listener da localização
+  void _addLocationListener(){
+    var locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 10
+    );
+    Geolocator.getPositionStream(locationSettings: locationSettings).listen((Position position){
+      _cameraPosition = CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 19,
+        );
+        _moveCamera(_cameraPosition);
+    });
+    
+  }
+
+
+  //Permissões de geolocalização
+  Future<void> _checkPermissions() async{
+    bool servicesEnabled;
+    LocationPermission permission;
+    servicesEnabled = await Geolocator.isLocationServiceEnabled();
+    if(!servicesEnabled){
+      return Future.error("Os serviços de localização estão desativados");
+    }
+    permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied){
+      permission = await Geolocator.requestPermission();
+      if(permission == LocationPermission.denied){
+        return Future.error("Permissões negadas");
+      }
+    }
+    if(permission == LocationPermission.deniedForever){
+      return Future.error("Permissões totalmente negadas. Impossível prosseguir");
+    }
+  }
+
+
+  //Movimentação da camera  
+  Future<void> _moveCamera(CameraPosition cameraPosition) async{
+    GoogleMapController googleMapController = await _mapController.future;
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(cameraPosition)
+    );
+  }
+
+  //Inicializando os métodos do localização
+  Future<void> _initLocation() async{
+    await _checkPermissions();
+    await _getLastKnownPositon();
+    _addLocationListener();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initLocation();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -68,11 +147,9 @@ class _PassengerPanelState extends State<PassengerPanel> {
         padding: EdgeInsets.only(bottom: 2),
         child: GoogleMap(
           mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(-23.472297, -46.530986),
-            zoom: 16
-          ),
+          initialCameraPosition: _cameraPosition,
           onMapCreated: _onMapCreated,
+          myLocationEnabled: true,
         ),
       ),
     );
