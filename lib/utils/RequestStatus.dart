d3e9a2e01a5ea7d04d7c736239b4f693f97ad58b
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-03-26 17:18:17 -0300

    :sparkles:feat: Classe utilitária para os status da requisição

---
+++ lib/utils/RequestStatus.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: file_names, 
+
+class RequestStatus {
+  static final aguardando = "aguardando";
+  static final aCaminho = "A_caminho";
+  static final viagem = "viagem";
+  static final finalizada = "finalizada";
+  
+}
