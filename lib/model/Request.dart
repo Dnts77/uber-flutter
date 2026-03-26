2b01289899eae82c50e9282f60f0ab791c72a23d
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-03-26 17:17:01 -0300

    :sparkles:feat: Modelo de requisição

---
+++ lib/model/Request.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: file_names, unnecessary_getters_setters
+import 'package:uber_flutter/model/Destiny.dart';
+import 'package:uber_flutter/model/Usuario.dart';
+
+class Request {
+  late String _id;
+  late String _status;
+  late Usuario _passageiro;
+  late Usuario _motorista;
+  late Destiny _destino;
+  
+  Request();
+  
+  String get id => _id;
+  set id(String value) => _id = value;
+
+  String get status => _status;
+  set status( String value) => _status = value;
+
+  Usuario get passageiro => _passageiro;
+  set passageiro( Usuario value) => _passageiro = value;
+
+  Usuario get motorista => _motorista;
+  set motorista( Usuario value) => _motorista = value;
+
+  Destiny get destino => _destino;
+  set destino( Destiny value) => _destino = value;
+
+  Map<String, dynamic> toMap(){
+
+    Map<String, dynamic> passengerData = {
+      "nome" : passageiro.nome,
+      "email" : passageiro.email,
+      "tipoUsuario": passageiro.tipoUsuario,
+      "idUsuario" : passageiro.idUsuario,
+    };
+
+    Map<String, dynamic> destinyData = {
+      "rua" : destino.rua,
+      "numero" : destino.numero,
+      "bairro": destino.bairro,
+      "cep" : destino.cep,
+      "latitude" : destino.latitude,
+      "longitude" : destino.longitude,
+    };
+
+    Map<String, dynamic> requestData = {
+      "status" : status,
+      "passageiro" : passengerData,
+      "motorista": null,
+      "destino" : destinyData,
+    };
+    return requestData;
+  }
+
+}
